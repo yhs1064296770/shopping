36b9411b774b6813fe76fa2d07edc67d5b4a663e
yhs1064296770 <jgkj@pb6a80a39.tokynt01.ap.so-net.ne.jp>
2016-05-06 11:10:08 +0800

    main
    1

---
+++ YhsShopping/YhsShopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YhsShopping
+//
+//  Created by 姚海深 on 16/5/6.
+//  Copyright © 2016年 姚海深. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YhsShopping/YhsShopping/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YhsShopping
+//
+//  Created by 姚海深 on 16/5/6.
+//  Copyright © 2016年 姚海深. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
